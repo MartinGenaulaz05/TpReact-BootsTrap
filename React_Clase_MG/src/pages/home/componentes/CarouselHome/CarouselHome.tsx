5484d7819f1f7ce371e52afc117dc40ad1755905
MartinGenaulaz05 <142841499+MartinGenaulaz05@users.noreply.github.com>
2023-10-28 22:44:55 -0300

    TP_React_TS

--- React_Clase_MG/src/pages/home/componentes/CarouselHome/CarouselHome.tsx
+++ React_Clase_MG/src/pages/home/componentes/CarouselHome/CarouselHome.tsx
@@ -6,7 +6,7 @@ const CauroselHome = () => {
       <img 
       className='d-block w-100' 
       style={{maxHeight:"500px", objectFit:'cover'}}
-      src='./src/assets/images/ropa-segunda-mano-usada-barata-1200x640.jpg.jpg' alt='cargando'/>
+      src='https://www.tuexperto.com/wp-content/uploads/2020/06/ropa-segunda-mano-usada-barata-1080x720.jpg.webp' alt='cargando'/>
       <Carousel.Caption>
         <h3>Tienda de Ropa</h3>
         <p>Buzos</p>
@@ -16,7 +16,7 @@ const CauroselHome = () => {
       <img 
         className='d-block w-100' 
         style={{maxHeight:"500px", objectFit:'cover'}}
-        src='./src/assets/images/pantaloneCarrusel.jpg' alt='cargando'/>
+        src='https://img.freepik.com/fotos-premium/pantalones-cortos-jeans-estante-tienda-ropa-moda-estantes-tienda_130265-4797.jpg?w=996' alt='cargando'/>
       <Carousel.Caption>
         <h3>Tienda de Ropa</h3>
         <p>Pantalones</p>
@@ -26,7 +26,7 @@ const CauroselHome = () => {
       <img 
         className='d-block w-100' 
         style={{maxHeight:"500px", objectFit:'cover'}}
-        src='./src/assets/images/tienda-ropa-tienda-ropa-perchas-tienda-boutique-moderna_1150-8886.jpg' alt='cargando'/>
+        src='https://img.freepik.com/fotos-premium/tienda-moda-presenta-ropa-perchas-que-incorporan-estetica-boutique-moderna_896558-6197.jpg?w=900' alt='cargando'/>
       <Carousel.Caption>
         <h3>Tienda de Ropa</h3>
         <p>
