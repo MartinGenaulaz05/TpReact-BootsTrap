0b0d0252d9c82464ec51c736a33ba2433cb30e6b
MartinGenaulaz05 <142841499+MartinGenaulaz05@users.noreply.github.com>
2023-10-28 22:43:58 -0300

    TP_React_TS

--- React_Clase_MG/src/pages/home/componentes/AboutUs/AboutUs.tsx
+++ React_Clase_MG/src/pages/home/componentes/AboutUs/AboutUs.tsx
@@ -26,7 +26,7 @@ const AboutUs = () => {
           </Card.Body>
         </Card>
         <Card style={{ width: '18rem', margin: '50px', backgroundColor:"#DCBB9B" }}>
-          <Card.Img variant="top" src="./src/assets/images/remera2.jpg" alt='cargando' style={{ maxHeight: "300px", minHeight: "100px" }} />
+          <Card.Img variant="top" src="https://acdn.mitiendanube.com/stores/029/842/products/nirvana1-2d1bbed349758198e516166141022282-640-0.webp" alt='cargando' style={{ maxHeight: "300px", minHeight: "100px" }} />
           <Card.Body>
             <Card.Title>Remera</Card.Title>
             <Card.Text>
@@ -39,7 +39,7 @@ const AboutUs = () => {
           </Card.Body>
         </Card>
         <Card style={{ width: '18rem', margin: '50px', backgroundColor:"#DCBB9B"}}>
-          <Card.Img variant="top" src="./src/assets/images/Buzo-Off-White-1.jpg" alt='cargando' style={{ maxHeight: "300px", minHeight: "100px" }} />
+          <Card.Img variant="top" src="https://tugroovy.com/wp-content/uploads/2023/03/Buzo-Off-White-1.jpg" alt='cargando' style={{ maxHeight: "300px", minHeight: "100px" }} />
           <Card.Body>
             <Card.Title>Buzo</Card.Title>
             <Card.Text>
